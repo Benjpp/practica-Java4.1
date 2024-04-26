6dc80309e02d0553f2f459825d0c6366decb5cc6
Benjpp <perezperezbenjamin61@gmail.com>
2024-04-26 19:53:55 +0200

    Add PruebaAsignatura.java file

---
+++ src/main/java/PruebaAsignatura.java
@@ -0,0 +1,8 @@
+import notas.*;
+public class PruebaAsignatura {
+	static final String[] als= {"12455666F;Lopez Perez, Pedro;6.7","33678999D;Merlo Gomez, Isabel;5.8","23555875G;Martinez Herrera, Lucia;9.1"};
+	public static void main(String[] args) {
+		Asignatura asignatura=new Asignatura("PA1",als);
+		
+	}
+}
