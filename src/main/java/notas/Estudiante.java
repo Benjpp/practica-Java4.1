9e8a1fb5ef4e174efbb1968a2c85d26b7d354677
Benjpp <perezperezbenjamin61@gmail.com>
2024-04-26 11:15:24 +0200

    Add Estudiante.java file

--- src/main/java/notas/Estudiante.java
+++ src/main/java/notas/Estudiante.java
@@ -1,5 +1,23 @@
 package notas;
 
 public class Estudiante {
-
+	private String dni;
+	private String nombre;
+	private double nota;
+	public Estudiante(String dni, String nombre) {
+		this.dni=dni;
+		this.nombre=nombre;
+		this.nota=0;
+	}
+	public Estudiante(String dni, String nombre, double nota) {
+		this.dni=dni;
+		this.nombre=nombre;
+		this.nota=nota;
+	}
+	public String getNombre() {
+		return this.nombre;
+	}
+	public String getDni() {
+		return this.dni;
+	}
 }
--- src/main/java/notas/EstudianteException.java
+++ src/main/java/notas/EstudianteException.java
@@ -1,5 +1,11 @@
 package notas;
 
-public class EstudianteException {
-
+public class EstudianteException extends Exception{
+	public EstudianteException() {
+		super();
+	}
+	public EstudianteException(String mensaje) {
+		super(mensaje);
+		
+	}
 }
